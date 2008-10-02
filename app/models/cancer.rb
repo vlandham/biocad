77abb4bed75648e2bf6fadd6a46e06a29e729b81
vlandham <landham@gmail.com>
2008-10-01 22:02:03 -0500

    cleaned up stylesheet.  moved stuff around

--- app/models/cancer.rb
+++ app/models/cancer.rb
@@ -1,7 +1,11 @@
 class Cancer < ActiveRecord::Base
   has_many :gene_types #needed for the through to work?
   has_many :genes, :through => :gene_types
+  has_many :onco_genes, :through => :gene_types, :source => :gene, :conditions => ['gene_types.association = ?', 'onco']
+  has_many :suppressor_genes, :through => :gene_types, :source => :gene, :conditions => ['gene_types.association = ?', 'suppressor']
   has_many :experiments
+  
+  
   
   validates_uniqueness_of :name
   
--- app/models/gene.rb
+++ app/models/gene.rb
@@ -2,11 +2,14 @@ class Gene < ActiveRecord::Base
   acts_as_network :gene_interactions, :through => :interactions
   
   has_many :cancers, :through => :gene_types
+  has_many :onco_cancers, :through => :gene_types, :source => :cancer, :conditions => ['gene_types.association = ?', 'onco']
+  has_many :suppressed_cancers, :through => :gene_types, :source => :cancer, :conditions => ['gene_types.association = ?', 'suppressor']
   has_many :gene_types
   has_and_belongs_to_many :pathways
   has_many :synonyms
   
   validates_uniqueness_of :gene_symbol
+  
   
   def to_s
     self.gene_symbol
