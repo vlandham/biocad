868a6e14fa0f636462267888dcf043daa068e6fe
vlandham <landham@gmail.com>
2008-10-01 23:13:05 -0500

    started adding search to genes

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,10 @@
 class SearchesController < ApplicationController
+  def index
+    @cancers = Cancer.find(:all)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @genes }
+    end
+  end
 end
