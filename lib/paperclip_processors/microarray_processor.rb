c5ab5f95bfd50dfbab69efc9e6f432b31e493407
Jim V <landham@gmail.com>
2009-02-06 07:34:02 -0600

    added todo

--- lib/paperclip_processors/microarray_processor.rb
+++ lib/paperclip_processors/microarray_processor.rb
@@ -13,6 +13,7 @@ module Paperclip
       lines = file.readlines[1...-1]
       lines.each do |line|
         dst << line.gsub(/^(\S+)\s+/, "")
+        # TODO: take $1 and stick them into a new file to store the gene names.  should have same base path as the temp file we're making.
       end
       dst
     end
