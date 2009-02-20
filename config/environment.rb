ab9788aa13b86a0c64603cf11f0b18ea5b74045e
vlandham <landham@gmail.com>
2009-02-20 15:20:19 -0600

    took out the will_paginate. Now it will be installed as a plugin. Fixed deprecated call to partial in genes index.

--- config/environment.rb
+++ config/environment.rb
@@ -26,7 +26,9 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
-
+  config.gem 'mislav-will_paginate', :version => '~> 2.2.3', :lib => 'will_paginate', 
+    :source => 'http://gems.github.com'
+  
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
