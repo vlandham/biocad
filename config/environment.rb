6ed9b7d72ac30f637d9b4c1c1965f87da24d4e19
vlandham <landham@gmail.com>
2009-04-08 15:25:55 -0500

    added fastercsv to gem requirements

--- config/environment.rb
+++ config/environment.rb
@@ -28,6 +28,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'mislav-will_paginate', :version => '~> 2.3.7', :lib => 'will_paginate', 
     :source => 'http://gems.github.com'
+  config.gem 'fastercsv', :version => '~> 1.4.0'
   
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
